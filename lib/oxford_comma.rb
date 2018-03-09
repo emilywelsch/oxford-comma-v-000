a0c23fe5c9f338387386f53551d690b4fcf28cdb
Emily Welsch <emwelsch@gmail.com>
2018-03-09 20:18:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,9 +7,9 @@ def oxford_comma(array)
   if array.count == 2
     array.join(" and ")
   else array.count >= 3
-    array.insert(array.count-1,"and")
     array.join(", ")
-  end
+    array.insert(array.count-1,"and")
+    end
 end
 oxford_comma(array)
 # "fiddleheads, okra, and kohlrabi"
