259954088ff34d4224a5ecd7cdf97896d83f05cd
Emily Welsch <emwelsch@gmail.com>
2018-03-09 19:36:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 array = ["fiddleheads","okra","kohlrabi"]
 # "fiddleheads, okra, and kohlrabi"
+def new_array
+  array << "and"
+end
+
 def oxford_comma(array)
-  array.join(", ")
-  array[1] << "and"
+  new_array.join(", ")
 end
