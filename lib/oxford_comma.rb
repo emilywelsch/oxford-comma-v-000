d155806f591a13df9b651f44bb6c8ab78eea9844
Emily Welsch <emwelsch@gmail.com>
2018-03-09 20:23:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,8 @@ def oxford_comma(array)
   if array.count == 2
     array.join(" and ")
   else array.count >= 3
-    array.join(", ")
     array.insert(array.count-1,"and")
+    array.join(", ")
     end
 end
 oxford_comma(array)
