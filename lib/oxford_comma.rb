15e7132c7d6fb2be3e44e0bd4e789b0687d1ddb1
Emily Welsch <emwelsch@gmail.com>
2018-03-09 20:02:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
-array = ["fiddleheads","okra","kohlrabi"]
+array = ["this","that"]
+# array = ["fiddleheads","okra","kohlrabi"]
+# array = ["fiddleheads","okra","kohlrabi", "emily"]
 # array.push("and ")
 array << "and "
 def oxford_comma(array)
-  array.join(", ")
+  if array.count = 2
+    array.join(" and ")
+  end
+  # array.join(", ")
 end
 # oxford_comma(array)
 # "fiddleheads, okra, and kohlrabi"
